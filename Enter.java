1b46bbf16e5e415305bda03342f37c825317af05
dreams-wait <67752332+dreams-wait@users.noreply.github.com>
2020-08-26 23:32:11 +0800

    Add files via upload

---
+++ Enter.java
@@ -0,0 +1,17 @@
+
+public class Enter {
+	String name;
+	String cname;
+	String num;
+	public Enter()
+	{
+		
+	}
+	public Enter(String inname,String incname,String innum)
+	{
+		this.name = inname;
+		this.cname = incname;
+		this.num = innum;
+	}
+
+}
